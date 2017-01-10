62dfe8f81f3915fbe2ac187f70414a843390ffac
Kristopher Corso <Kristopher@disabledperson.com>
2017-01-10 11:53:17 -0800

    fixed error hopefully

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,14 +1,1 @@
-class Page < ActiveRecord::Base
 
-    require 'nokogiri'
-    require 'HTTParty'
-
-    page = HTTParty.get("https://www.disabledperson.com")
-
-    parse_page = Nokogiri::HTML(page)
-
-    parse_page.css('.content').css('a').each do |a|
-      Page.create(:scrape_categories a.text)
-    end
-
-end
